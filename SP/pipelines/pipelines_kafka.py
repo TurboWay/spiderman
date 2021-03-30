0759cb88d47faf6266a55446a97af1175f1271a5
way <way>
2021-03-30 16:18:18 +0800

    kafka 管道优化

--- SP/pipelines/pipelines_kafka.py
+++ SP/pipelines/pipelines_kafka.py
@@ -9,6 +9,7 @@ import json
 import time
 import logging
 from kafka import KafkaProducer
+from random import choice
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +19,14 @@ class KafkaPipeline(object):
     @classmethod
     def from_crawler(cls, crawler):
         settings = crawler.settings
-        return cls(**settings)
+        name = crawler.spider.name
+        return cls(name, **settings)
 
-    def __init__(self, **kwargs):
+    def __init__(self, name, **kwargs):
         self.kafkaproducer = KafkaProducer(bootstrap_servers=kwargs.get('KAFKA_SERVERS'),
                                            key_serializer=lambda m: m.encode('utf-8'),
                                            value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+        self.partitions = list(self.kafkaproducer.partitions_for(name)) # 获取所有分区
 
     def process_item(self, item, spider):
         """
@@ -34,7 +37,7 @@ class KafkaPipeline(object):
         new_item = {key: value for key, value in item.items()}
         new_item['ctime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         try:
-            self.kafkaproducer.send(topic=spider.name, key=item.tablename, value=new_item).get(timeout=10)
+            self.kafkaproducer.send(topic=spider.name, partition=choice(self.partitions), key=item.tablename, value=new_item).get(timeout=10)
             logger.info(f"入库成功 <= 主题:{spider.name} key名:{item.tablename}")
         except Exception as e:
             logger.error(f"入库失败 <= 主题:{spider.name} key名:{item.tablename} 错误原因:{e}")
